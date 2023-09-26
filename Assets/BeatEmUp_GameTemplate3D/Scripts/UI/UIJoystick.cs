b1c4bd69c2a3b37a019edf5ec84844c21bf86603
Maxim Copets <max91194091@gmail.com>
2023-09-26 16:12:27 +0300

    Buttons visualization

--- Assets/BeatEmUp_GameTemplate3D/Scripts/UI/UIJoystick.cs
+++ Assets/BeatEmUp_GameTemplate3D/Scripts/UI/UIJoystick.cs
@@ -2,80 +2,121 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Scripting;
 
 [RequireComponent(typeof(RectTransform))]
-public class UIJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
-	
-	public RectTransform handle;
-	public float radius = 40f;
-	public float autoReturnSpeed = 8f;
-	private bool returnToStartPos;
-	private RectTransform parentRect;
-	private InputManager inputmanager;
+public class UIJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
 
-	void OnEnable(){
-		returnToStartPos = true;
-		handle.transform.SetParent(transform);
-		parentRect = GetComponent<RectTransform>();
-	}
-		
-	void Update() {
-		if(inputmanager == null) inputmanager = GameObject.FindObjectOfType<InputManager>();
+    public RectTransform handle;
+    public float radius = 40f;
+    public float autoReturnSpeed = 8f;
+    private bool returnToStartPos;
+    private RectTransform parentRect;
+    private InputManager inputmanager;
 
-		//return to start position
-		if (returnToStartPos) {
-			if (handle.anchoredPosition.magnitude > Mathf.Epsilon) {
-				handle.anchoredPosition -= new Vector2 (handle.anchoredPosition.x * autoReturnSpeed, handle.anchoredPosition.y * autoReturnSpeed) * Time.deltaTime;
-				inputmanager.OnTouchScreenJoystickEvent(Vector2.zero);
-			} else {
-				returnToStartPos = false;
-			}
-		}
-	}
+    void OnEnable()
+    {
+        returnToStartPos = true;
+        handle.transform.SetParent(transform);
+        parentRect = GetComponent<RectTransform>();
+    }
 
-	//return coordinates
-	public Vector2 Coordinates {
-		get	{
-			if (handle.anchoredPosition.magnitude < radius){
-				return handle.anchoredPosition / radius;
-			}
-			return handle.anchoredPosition.normalized;
-		}
-	}
+    void Update()
+    {
+        if (inputmanager == null) inputmanager = GameObject.FindObjectOfType<InputManager>();
 
-	//touch down
-	void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
-		returnToStartPos = false;
-		var handleOffset = GetJoystickOffset(eventData);
-		handle.anchoredPosition = handleOffset;
-		if(inputmanager != null) inputmanager.OnTouchScreenJoystickEvent(handleOffset.normalized);
-	}
+        //return to start position
+        if (returnToStartPos)
+        {
+            if (handle.anchoredPosition.magnitude > Mathf.Epsilon)
+            {
+                handle.anchoredPosition -= new Vector2(handle.anchoredPosition.x * autoReturnSpeed, handle.anchoredPosition.y * autoReturnSpeed) * Time.deltaTime;
+                inputmanager.OnTouchScreenJoystickEvent(Vector2.zero);
+            }
+            else
+            {
+                returnToStartPos = false;
+            }
+        }
+        Vector2 visualDirection = new Vector2();
+        if (Input.GetKey(KeyCode.S))
+        {
+            visualDirection.y = -1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            visualDirection.y = 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            visualDirection.x = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            visualDirection.x = -1;
+        }
+        Visualizate(visualDirection);
 
-	//touch drag
-	void IDragHandler.OnDrag(PointerEventData eventData) {
-		var handleOffset = GetJoystickOffset(eventData);
-		handle.anchoredPosition = handleOffset;
-		if(inputmanager != null) inputmanager.OnTouchScreenJoystickEvent(handleOffset.normalized);
-	}
+    }
 
-	//touch up
-	void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
-		returnToStartPos = true;
-	}
+    private void Visualizate(Vector2 direction)
+    {
+        handle.anchoredPosition = new Vector2(radius * direction.x, radius * direction.y);
+    }
 
-	//get offset
-	private Vector2 GetJoystickOffset(PointerEventData eventData) {
-		
-		Vector3 globalHandle;
-		if (RectTransformUtility.ScreenPointToWorldPointInRectangle (parentRect, eventData.position, eventData.pressEventCamera, out globalHandle)) {
-			handle.position = globalHandle;
-		}
+    //return coordinates
+    public Vector2 Coordinates
+    {
+        get
+        {
+            if (handle.anchoredPosition.magnitude < radius)
+            {
+                return handle.anchoredPosition / radius;
+            }
+            return handle.anchoredPosition.normalized;
+        }
+    }
 
-		var handleOffset = handle.anchoredPosition;
-		if (handleOffset.magnitude > radius) {
-			handleOffset = handleOffset.normalized * radius;
-			handle.anchoredPosition = handleOffset;
-		}
-		return handleOffset;
-	}
+    //touch down
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        returnToStartPos = false;
+        var handleOffset = GetJoystickOffset(eventData);
+        handle.anchoredPosition = handleOffset;
+        if (inputmanager != null) inputmanager.OnTouchScreenJoystickEvent(handleOffset.normalized);
+    }
+
+    //touch drag
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        var handleOffset = GetJoystickOffset(eventData);
+        handle.anchoredPosition = handleOffset;
+        if (inputmanager != null) inputmanager.OnTouchScreenJoystickEvent(handleOffset.normalized);
+    }
+
+    //touch up
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        returnToStartPos = true;
+    }
+
+    //get offset
+    private Vector2 GetJoystickOffset(PointerEventData eventData)
+    {
+
+        Vector3 globalHandle;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(parentRect, eventData.position, eventData.pressEventCamera, out globalHandle))
+        {
+            handle.position = globalHandle;
+        }
+
+        var handleOffset = handle.anchoredPosition;
+        if (handleOffset.magnitude > radius)
+        {
+            handleOffset = handleOffset.normalized * radius;
+            handle.anchoredPosition = handleOffset;
+        }
+        return handleOffset;
+    }
 }
--- Assets/_Scripts/MyButtonEvent.cs
+++ Assets/_Scripts/MyButtonEvent.cs
@@ -5,12 +5,27 @@ using UnityEngine.EventSystems;
 
 public class MyButtonEvent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] KeyCode _keyCode = KeyCode.KeypadEnter;
     private Animation _animationClick;
-
+    private bool _isPressed;
     private void Start()
     {
         _animationClick = GetComponent<Animation>();
     }
+
+    private void FixedUpdate()
+    {
+        if( Input.GetKeyDown(_keyCode))
+        {
+            _animationClick.Play("click");
+            // _isPressed = true
+        }
+        if(Input.GetKeyDown(_keyCode))
+        {
+            _animationClick.Play("unclick");
+        }
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         _animationClick.Play("click");
